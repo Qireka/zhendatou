fba39a72f93da3d239c86fbb6c236a590de86a6d
Qireka <932203054@qq.com>
2019-11-07 14:25:10 +0800

    ckeditor实现博客后台富文本编辑，包括图片上传

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from ckeditor_uploader.fields import RichTextUploadingField
 
 
 class BlogType(models.Model):
@@ -16,7 +17,7 @@ class Blog(models.Model):
     # 创建博客模型
     title = models.CharField(max_length=50)     # 标题
     blog_type = models.ForeignKey(BlogType, on_delete=models.DO_NOTHING)        # 博客类型，从BlogType类导入
-    content = models.TextField()        # 内容
+    content = RichTextUploadingField()        # 内容
     author = models.ForeignKey(User, on_delete=models.DO_NOTHING)       # 作者,从User类下导入
     create_time = models.DateTimeField(auto_now_add=True)       # 创建时间
     last_updated_time = models.DateTimeField(auto_now=True)     # 最后修改时间
