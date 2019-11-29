a2770c78ef50e590cb2e32e8d3cf868f9f417a07
Qireka <932203054@qq.com>
2019-11-29 15:37:03 +0800

    做完树形评论回复结果出错了回档重弄，现在是正常状态，等待前端页面优化

--- blog/views.py
+++ blog/views.py
@@ -3,9 +3,10 @@ from django.core.paginator import Paginator
 from django.conf import settings
 from django.db.models import Count
 from django.contrib.contenttypes.models import ContentType
-from comment.models import Comment
+
 from .models import Blog, BlogType
 from read_statistics.utils import read_statistics_once_read
+from comment.models import Comment
 from comment.forms import CommentForm
 
 
@@ -20,6 +21,7 @@ def get_blog_list_common_data(request, blogs_all_list):
         page_range.insert(0, '...')
     if paginator.num_pages - page_range[-1] >= 2:
         page_range.append('...')
+    # 加上首页和尾页
     if page_range[0] != 1:
         page_range.insert(0, 1)
     if page_range[-1] != paginator.num_pages:
@@ -33,22 +35,14 @@ def get_blog_list_common_data(request, blogs_all_list):
                                          create_time__month=blog_date.month).count()
         blog_dates_dict[blog_date] = blog_count
 
-    # 获取博客分类的数量
-    """blog_types = BlogType.objects.all()
-    blog_types_list = []
-    for blog_type in blog_types:
-        blog_type.blog_count = Blog.objects.filter(blog_type=blog_type).count()
-        blog_types_list.append(blog_type)
-        """
     context = {}
     # 将blogs作为字典的键，所有博客作为值写入字典
     context['blogs'] = page_of_blogs.object_list
-    context['blog_types'] = BlogType.objects.annotate(blog_count=Count('blog'))
     context['page_of_blogs'] = page_of_blogs
-    context['blog_dates'] = blog_dates_dict
     context['page_range'] = page_range
+    context['blog_types'] = BlogType.objects.annotate(blog_count=Count('blog'))
+    context['blog_dates'] = blog_dates_dict
     return context
-
 
 def blog_list(request):
     blogs_all_list = Blog.objects.all()
@@ -79,16 +73,15 @@ def blog_detail(request, blog_pk):
     blog = get_object_or_404(Blog, pk=blog_pk)
     read_cookie_key = read_statistics_once_read(request, blog)
     blog_content_type = ContentType.objects.get_for_model(blog)
-    comments = Comment.objects.filter(content_type=blog_content_type, object_id=blog_pk)
+    comments = Comment.objects.filter(content_type=blog_content_type, object_id=blog.pk, parent=None)
 
     context = {}
     context['previous_blog'] = Blog.objects.filter(create_time__gt=blog.create_time).last()
     context['next_blog'] = Blog.objects.filter(create_time__lt=blog.create_time).first()
     context['blog'] = blog
-    context['comments'] = comments
-    context['comment_form'] = CommentForm(initial={'content_type': blog_content_type, 'object_id': blog_pk})
-    response = render(request, 'blog_detail.html', context)  # 相应
+    context['comments'] = comments.order_by('-comment_time')
+    context['comment_form'] = CommentForm(
+        initial={'content_type': blog_content_type, 'object_id': blog_pk, 'reply_comment_id': 0})
+    response = render(request, 'blog_detail.html', context)  # 响应
     response.set_cookie(read_cookie_key, 'true')  # 阅读cookie标记
     return response
-
-
